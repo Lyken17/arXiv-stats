ac41f2ef04b5dbb6b6e1f485c4b37aa96d6caf13
Ligeng Zhu <Lyken17@users.noreply.github.com>
2025-03-04 13:37:41 +0800

    push fixs

--- arxiv_daily_statistics.py
+++ arxiv_daily_statistics.py
@@ -25,8 +25,20 @@ else:
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(html, 'html.parser')
 
-s = soup.find(class_="list-dateline").text.split("announced")[-1].strip()
-day, month, year = [_.strip() for _ in s.split(",")[-1].split()]
+# s = soup.find(class_="list-dateline").text.split("announced")[-1].strip()
+# day, month, year = [_.strip() for _ in s.split(",")[-1].split()]
+
+from datetime import datetime
+import pytz
+
+# Get current date/time in Eastern timezone
+eastern = pytz.timezone('US/Eastern')
+eastern_time = datetime.now(eastern)
+year = eastern_time.strftime("%Y")
+month = eastern_time.strftime("%B")
+day = eastern_time.strftime("%d")
+
+
 info_folder = "info/%s-%s/%s" % (year, month, day)
 os.makedirs(info_folder, exist_ok=True)
 
@@ -42,8 +54,8 @@ for dl in soup.find_all("dl"):
 
     for (dd, dt) in dds_dts:
         # arXiv id
-        list_identifier = dt.find(class_="list-identifier")
-        id = list_identifier.a.text
+        # list_identifier = dt.find(class_="list-identifier")
+        arxiv_id = dt.find(title="Abstract").attrs["id"]
 
         # title
         list_title = dd.find_all(class_="list-title")[0]
@@ -54,23 +66,24 @@ for dl in soup.find_all("dl"):
         # authors
         list_authors = dd.find_all(class_="list-authors")
         authors = [_.text for _ in list_authors[0].find_all("a")]
-
+        authors = [" ".join(_.split()) for _ in authors]
         # subjectives / areas
         list_subjects = dd.find_all(class_="list-subjects")[0]
         if list_subjects.span.attrs["class"] == ["descriptor"]:
             list_subjects.span.extract()
-        subjectives = list_subjects.text.strip()
+        subjectives = list_subjects.text.replace("\n", "").strip()
+        subjectives = " ".join(subjectives.split())
         subjectives = [_.strip() for _ in subjectives.split(";")]
 
         paper_info = {
-            "id": id,
+            "id": arxiv_id,
             "title": title,
             "authors": authors,
             "subjectives": subjectives
         }
         information.append(paper_info)
-
-
+        # print(paper_info)
+        # break
 
 with open("%s/paper_info.json" % info_folder, "w") as fp:
     json.dump(information, fp, indent=2)
